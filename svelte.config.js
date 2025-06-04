e95671ff99aa563450daaf3e4f5ecfa662df01e4
Hasha Dar <43448050+hashadar@users.noreply.github.com>
2025-06-04 12:45:55 +0100

    node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto'; // Comment out or remove
+import adapter from '@sveltejs/adapter-node';    // Add this
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            // Default output directory is 'build'.
+            // You can specify 'out: "build"' if you want to be explicit
+        })
+    }
 };
-
 export default config;
